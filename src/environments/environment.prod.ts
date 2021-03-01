ab70a301353572b25a59d33051ee1e082f516cc3
Nitheesh T Ganesh <nitheesh.ganesh@aot-technologies.com>
2021-02-28 20:16:03 -0800

    new changes 4

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,8 @@
+const BASE_URL = "https://u3pgbxg4gg.execute-api.us-east-1.amazonaws.com/"
+
 export const environment = {
-  production: true
+  production: true,
+  authURL: BASE_URL + 'auth/v1',
+  customerURL: BASE_URL + 'customer/v1',
+  performanceURL: BASE_URL + 'performance/v1',
 };
